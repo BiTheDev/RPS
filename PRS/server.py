637859a2d5a70d48c908f55fdc240d3d4c26f7f2
Brian Ho <37948148+Phantom30@users.noreply.github.com>
2018-07-06 11:17:17 -0700

    Add files via upload

---
+++ PRS/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html")
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    # Notice how the key we are using to access the info corresponds with the names
+    # of the inputs from our html form
+    name = request.form['name']
+    email = request.form['email']
+    return redirect('/') # redirects back to the '/' route
+
+@app.route('/paper')
+def show_paper():
+    return render_template('paper.html')
+
+@app.route('/rock')
+def show_rock():
+    return render_template('rock.html') 
+
+@app.route('/scissors')
+def show_scissors():
+    return render_template('Scissors.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
